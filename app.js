ad9edfb056f458f7579c7a08d151065289f0a27e
narender saini <narendersaini32@gmail.com>
2019-05-15 11:26:24 +0530

    basic socket added

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import http from 'http';
+import Socket from 'socket.io';
 
+const server = http.Server(express);
+const io = Socket(server);
 const app = express();
 const port = 4000;
 
 app.get('/', (req, res) => res.send('old'));
-
-app.listen(port, () => console.log('Chat app Server Started'));
+io.on('connection', (socket) => {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', (data) => {
+    console.log(data);
+  });
+});
+server.listen(port, () => console.log('Chat app Server Started'));
